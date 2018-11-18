716878733d827992d3657431aa7cf537aacd0d40
Hansel De La Cruz <hansel809@hotmail.com>
2018-11-18 16:56:23 -0500

    clean up build system and add unit tests

---
+++ src/log.unittest.cpp
@@ -0,0 +1,37 @@
+/**
+ *  This file is part of Sage.
+ *
+ *  Copyright (C) 2018  Hansel De La Cruz
+ *
+ *  Sage is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Sage is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Sage.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <catch2/catch.hpp>
+#include <sage/core/log.hpp>
+
+TEST_CASE("can use log", "[sage][core][log]") {
+    LOG_INTERNAL_TRACE("trace");
+    LOG_INTERNAL_DEBUG("debug");
+    LOG_INTERNAL_INFO("info");
+    LOG_INTERNAL_WARN("warn");
+    LOG_INTERNAL_ERROR("error");
+    LOG_INTERNAL_CRITICAL("critical");
+
+    LOG_TRACE("trace");
+    LOG_DEBUG("debug");
+    LOG_INFO("info");
+    LOG_WARN("warn");
+    LOG_ERROR("error");
+    LOG_CRITICAL("critical");
+}
---
+++ tests/main.cpp
@@ -0,0 +1,27 @@
+/**
+ *  This file is part of Sage.
+ *
+ *  Copyright (C) 2018  Hansel De La Cruz
+ *
+ *  Sage is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Sage is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Sage.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define CATCH_CONFIG_RUNNER
+#include <catch2/catch.hpp>
+#include <sage/sage.hpp>
+
+auto main(int argc, char *argv[]) -> int {
+    sg::log::init();
+    return Catch::Session().run(argc, argv);
+}
